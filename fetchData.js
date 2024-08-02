2e75de1488b40e70941ba9579da74a981a1a9984
Pankaj Papnai <70071875+p-papnai@users.noreply.github.com>
2024-08-02 13:27:57 +0530

    Update fetchData.js

--- fetchData.js
+++ fetchData.js
@@ -1,22 +1,17 @@
-// fetchData.js
-
 const axios = require('axios');
-const fs = require('fs').promises;
-require('dotenv').config();
-
+const fs = require('fs/promises');
 
 async function fetchData() {
   try {
+    const apiKey = process.env.KEY; // Get the API key from environment variables
 
-     const apiKey = process.env.KEY; // Get the API key from environment variables
+    if (!apiKey) {
+      console.error('API_KEY is not defined in the environment variables');
+      return;
+    }
 
-  if (!apiKey) {
-    console.error('API_KEY is not defined in the environment variables');
-    return;
-  }
-
-    
-    const response = await axios.get("https://v6.exchangerate-api.com/v6/${apiKey}/latest/INR");
+    // Use backticks for template literals
+    const response = await axios.get(`https://v6.exchangerate-api.com/v6/${apiKey}/latest/INR`);
     const data = response.data;
     
     // Save data to a local file (or database, as per your requirement)
